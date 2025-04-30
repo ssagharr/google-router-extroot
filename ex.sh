33873f39bfd8c8c69411a931d356c6f1c0a6b576
ssagharr <saeid.sagharchi@gmail.com>
2025-04-30 17:06:26 +0330

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,11 +1,9 @@
 #!/bin/sh  
 set -e  # Exit immediately if a command exits with a non-zero status  
-# Variables
-DISK="/dev/mmcblk0"
-DEVICE="${DISK}p3"  
+  
 
 # Script Introduction  
-echo -e "\033[1;36mThis script is developed by ssagharr for the \033[1;34mGoogle Wifi AC-1304\033[1;36m \033[32mOpenWRT\033[1;36m devices.\033[0m"  
+echo -e "\033[1;36mThis script is developed for the \033[1;34mGoogle Wifi AC-1304\033[1;36m \033[32mOpenWRT\033[1;36m devices.\033[0m"  
 echo -e "\033[1;36mIt will configure extroot to expand the storage by using the free space on the internal storage to create an extra partition for overlay data.\033[0m"  
 echo -e "\033[1;33mDo you want to continue? [\033[32my\033[0m/\033[31mn\033[0m]\033[0m"  
 read -r answer  
@@ -13,7 +11,9 @@ if [ "$answer" != "y" ]; then
   echo -e "\033[31mExiting the script.\033[0m"  
   exit 0  
 fi  
-
+# Variables
+DISK="/dev/mmcblk0"
+DEVICE="${DISK}p3"
   
 # 3-second delay before Step 1  
 sleep 3  
@@ -21,107 +21,89 @@ sleep 3
 # Step 1: Install required packages  
 echo -e "\033[34mUpdating and installing required packages...\033[0m"  
 opkg update  
-opkg install block-mount kmod-fs-ext4 e2fsprogs parted kmod-usb-storage ncat pv gzip  coreutils-dd coreutils-mv openssh-sftp-server lsblk curl #luci-app-attendedsysupgrade luci cfdisk resize2fs 
+opkg install block-mount kmod-fs-ext4 e2fsprogs parted kmod-usb-storage luci-app-attendedsysupgrade luci cfdisk resize2fs ncat pv gzip  coreutils-dd coreutils-mv openssh-sftp-server lsblk
   
 # Step 2: Create new partition  
 echo -e "\033[1;33mDo you want to create a new partition for extroot? [\033[32my\033[0m/\033[31mn\033[0m]\033[0m"  
 read -r answer  
 if [ "$answer" = "y" ]; then  
   echo -e "\033[34mCreating a new partition for extroot...\033[0m"  
-  # Variables
-#DISK="/dev/mmcblk0"
-sleep 3
   parted -s ${DISK} -- mkpart extroot 247808s -2048s  
 else  
   echo -e "\033[35mSkipping partition creation.\033[0m"  
 fi  
-  sleep 5
+  
 # Step 3: Format the new partition  
 echo -e "\033[1;33mDo you want to format the new partition? [\033[32my\033[0m/\033[31mn\033[0m]\033[0m"  
 read -r answer  
 if [ "$answer" = "y" ]; then  
-  echo -e "\033[34mFormatting the new partition...\033[0m" 
-  # Variables
-#DEVICE="${DISK}p3"
-sleep 3
+  echo -e "\033[34mFormatting the new partition...\033[0m"  
   mkfs.ext4 -L extroot ${DEVICE}  
 else  
   echo -e "\033[31mSkipping partition formatting and exiting.\033[0m"  
   exit 0  
 fi  
   
-# 5-second delay before Step 4  
-sleep 5 
-# Variables
-DISK="/dev/mmcblk0"
-DEVICE="${DISK}p3"
+# 3-second delay before Step 4  
+sleep 3  
+
 # Step 4: Configure extroot  
 echo -e "\033[34mConfiguring extroot...\033[0m"
-sleep 5  
+sleep 3  
 eval $(block info ${DEVICE} | grep -o -e 'UUID="\S*"') 
-sleep 5
-#eval $(block info ${DEVICE} | grep -o -e 'LABEL="\S*"')
-#sleep 5
-#echo -e "\033[35mOk .\033[0m" 
-eval $(block info | grep -o -e 'MOUNT="\S*/overlay"')
-#echo -e "\033[35mOk . .\033[0m" 
-#set +e
-sleep 5
-uci -q delete fstab.extroot
-#echo -e "\033[35mOk . . .\033[0m" 
+echo -e "\033[35mOk .\033[0m" 
 sleep 3
-#set -e
+eval $(block info | grep -o -e 'MOUNT="\S*/overlay"')
+echo -e "\033[35mOk . .\033[0m" 
+sleep 3 
+uci -q delete fstab.extroot 
+echo -e "\033[35mOk . . .\033[0m" 
 sleep 3
 uci set fstab.extroot="mount"
-#echo -e "\033[35mOk . . . .\033[0m"
-sleep 5
+echo -e "\033[35mOk . . . .\033[0m"
+sleep 3 
 uci set fstab.extroot.uuid="${UUID}"
-sleep 5
-#uci set fstab.extroot.label="${LABEL}"
-#sleep 5
-#echo -e "\033[35mOk . . . . .\033[0m"
-sleep 5  
+echo -e "\033[35mOk . . . . .\033[0m"
+sleep 3  
 uci set fstab.extroot.target="${MOUNT}"  
-#echo -e "\033[35mOk . . . . . .\033[0m"
+echo -e "\033[35mOk . . . . . .\033[0m"
   
 # Adding delay around uci commit fstab  
 echo -e "\033[1;36mCommitting changes to fstab...\033[0m"  
-sleep 7 
+sleep 5  
 uci commit fstab  
 echo -e "\033[1;36mChanges committed to fstab.\033[0m"  
-
-sleep 7 
+sleep 5  
   
 # Step 5: Configure rootfs_data  
-echo -e "\033[34mConfiguring rootfs_data...\033[0m"
-set +e
-sleep 2
+echo -e "\033[34mConfiguring rootfs_data...\033[0m"  
 ORIG="$(block info | sed -n -e '/MOUNT="\S*\/overlay"/s/:\s.*$//p')" 
 echo -e "\033[35mOk .\033[0m"
-sleep 6
+sleep 3
+set +e
 uci -q delete fstab.rwm 
 echo -e "\033[35mOk . .\033[0m"
-sleep 6
+sleep 3
+set -e
 uci set fstab.rwm="mount"
 echo -e "\033[35mOk . . .\033[0m"  
-sleep 6
+sleep 3
 uci set fstab.rwm.device="${ORIG}" 
 echo -e "\033[35mOk . . . .\033[0m"
-sleep 6
+sleep 3
 uci set fstab.rwm.target="/rwm"  
 echo -e "\033[35mOk . . . . .\033[0m"  
 # Adding delay around uci commit fstab  
 echo -e "\033[1;36mCommitting changes to fstab...\033[0m"  
-sleep 6  
+sleep 5  
 uci commit fstab  
-echo -e "\033[1;36mChanges committed to fstab.\033[0m"
-set -e
-sleep 6  
+echo -e "\033[1;36mChanges committed to fstab.\033[0m"  
+sleep 5  
   
 # Step 6: Transfer data  
 echo -e "\033[34mTransferring overlay data to the new partition...\033[0m"  
 mount ${DEVICE} /mnt  
-sleep 6
+sleep 5
 tar -C ${MOUNT} -cvf - . | tar -C /mnt -xf -  
 sleep 3
 # Initial message
